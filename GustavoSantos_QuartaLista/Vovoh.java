6cc23251d237a744fff87c5ffba40a30992059b9
Gustavo <110639365+ThorfDivine@users.noreply.github.com>
2023-06-07 17:55:48 -0300

    upload
    feito até o 3

---
+++ GustavoSantos_QuartaLista/Vovoh.java
@@ -0,0 +1,19 @@
+public class Vovoh {
+    public static void main(String[] args){
+        
+    }
+}
+class vovohs{
+    String corDVo;
+    int anosDVo; 
+    Float qntQFlt;
+    char inicialN; 
+    boolean vivaS;
+    public vovohs(String corVo, int anosVo, Float qntFlt, char inicial, boolean viva){
+        corDVo = corVo;
+        anosDVo = anosVo;
+        qntQFlt = qntFlt;
+        inicialN = inicial;
+        vivaS = viva;
+    }
+}
