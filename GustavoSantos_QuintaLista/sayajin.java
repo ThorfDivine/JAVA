e5a6c98ae564e09ff293e25ec34b030c49e3dba3
Gustavo <110639365+ThorfDivine@users.noreply.github.com>
2023-06-12 17:45:01 -0300

    100% atualizado

--- GustavoSantos_QuintaLista/sayajin.java
+++ GustavoSantos_QuintaLista/sayajin.java
@@ -4,18 +4,18 @@
 public class sayajin {
 
     public static void main(String[] args){
-        sayan goku = new sayan("goku", 50, 100.5, 'S', true);
+        sayan goku = new sayan("goku", 50, 100.5, 'S', true, "azul", "preto", "tom de laranja", 1.80, 10.0, 15.0);
         System.out.println(goku.classeDoSayajin);
         System.out.println(goku.nomeDoSayajin);
         System.out.println(goku.idadeDoSayajin);
-        System.out.println(goku.tamanhoCabelo);
-        System.out.println(goku.tamanhoCoxa);
+        System.out.println(goku.tamanhoDoCabelo);
+        System.out.println(goku.tamanhoDaCoxa);
         System.out.println(goku.tamanhoDoKi);
         System.out.println(goku.classeDoSayajin);
         System.out.println(goku.homem);
         System.out.println(goku.corDoBelo);
-        System.out.println(goku.corKI);
-        System.out.println(goku.corPele);
+        System.out.println(goku.corDoKi);
+        System.out.println(goku.corDaPele);
     }
 }
 
@@ -25,25 +25,25 @@ public class sayajin {
     double tamanhoDoKi;
     char classeDoSayajin;
     boolean homem;
-    String corKI;
-    String corBelo;
-    String corPele;
+    String corDoKi;
+    String corDoBelo;
+    String corDaPele;
     double altura;
-    double tamanhoCabelo;
-    double tamanhoCoxa;
+    double tamanhoDoCabelo;
+    double tamanhoDaCoxa;
 
-        sayan(String nomeSayajin, int idadeSayajin, double tamanhoKi, char classeSayajin, boolean eHomem, String corKI;String corDoBelo, String corDaPele, double alturaS, double tamanhoDoCabelo, double tamanhoDaCoxa){
+        sayan(String nomeSayajin, int idadeSayajin, double tamanhoKi, char classeSayajin, boolean eHomem, String corKi,String corBelo, String corPele, double alturas, double tamanhoCabelo, double tamanhoCoxa){
         this.nomeDoSayajin = nomeSayajin;
         this.idadeDoSayajin = idadeSayajin;
         this.tamanhoDoKi = tamanhoKi;
         this.classeDoSayajin = classeSayajin;
         this.homem = eHomem;
-         corKI;
-         corDoBelo;
-         corPele;
-         altura;
-         tamanhoCabelo;
-         tamanhoCoxa;
+        this.corDoKi =  corKi;
+        this.corDoBelo = corBelo;
+        this.corDaPele = corPele;
+        this.altura = alturas;
+        this.tamanhoDoCabelo = tamanhoCabelo;
+        this.tamanhoDaCoxa = tamanhoCoxa;
         }
     String elevarKI(){
         return "elevando";
