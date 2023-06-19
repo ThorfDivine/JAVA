cf266b19697fc8a87e16d45a1a2ee84e434260bb
Gustavo <110639365+ThorfDivine@users.noreply.github.com>
2023-06-19 16:41:54 -0300

    Update alimento.java

--- GustavoSantos_QuintaLista/alimento.java
+++ GustavoSantos_QuintaLista/alimento.java
@@ -3,7 +3,19 @@
  *Data: 07/06/2023
  */
 public class alimento{
-    public static void main(String[] args){}
+    public static void main(String[] args){
+         alimentos.corExterna= "laranja";
+         alimentos.corInterna= "laranja";
+         alimentos.tipoDeAlimento= "tuberculo"; 
+         alimentos.tiposDenutrientes= "C e V"; 
+         alimentos.textura= "firme";
+         alimentos.odor= null;
+         alimentos.paladio= "doce";
+         alimentos.peso= 0.5f;
+         alimentos.resistencia= 10;
+         alimentos.tamanho= 'G';
+         alimentos.comestivel= true;
+    }
 }
 class Alimentos{
         String corExterna, corInterna, tipoDeAlimento, tiposDenutrientes, textura, odor, paladio;
